3df437ec3d67d005cd00b38afe075da16c697872
Owada <owada@material.co.jp>
2026-06-02 17:53:06 +0900

    no message

---
+++ test2.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<!-- TemplateBeginEditable name="doctitle" -->
+<title>無題ドキュメント</title>
+<!-- TemplateEndEditable -->
+<!-- TemplateBeginEditable name="head" -->
+<!-- TemplateEndEditable -->
+</head>
+
+<body>
+<p>HELLO WORLD</p>
+</body>
+</html>
